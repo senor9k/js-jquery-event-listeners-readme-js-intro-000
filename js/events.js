e4ed53d13d9521d09db652ac1b82e3af3388fc31
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-15 19:39:28 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function() {
+      alert('Hey!');
+    });
+}
+function frameIt(){
+  $('img').on('load', function(){
+      $('img').addClass('tasty');
+    });
+}
+function pressIt(){
+  $("input#typing").on('keydown', function(e) {
+    if(e.which == 71){
+        alert('G was pressed');
+    }
+  });
+}
+function submitIt(){
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+    return;
+  });
+}
+
 
 $(document).ready(function(){
-
 // call functions here
-
+    getIt()
+    frameIt()
+    pressIt()
+    submitIt()
 });
