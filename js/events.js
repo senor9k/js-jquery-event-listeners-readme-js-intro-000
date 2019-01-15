53213e892db7edd25325bf5060c2b7b5f0e1478c
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-15 18:41:47 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,12 @@
 $(document).ready(function(){
 
 // call functions here
+function getIt(){
+  $(document).on('click', function(key) {
+  if(key.which == "p"){
+      alert('Hey!');
+  }
+});
+}
 
 });
